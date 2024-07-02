3014650d6868fed6aebdbedd57ddcf92b111a258
martinroz25@gmail.com <martinroz25@gmail.com>
2024-07-02 13:00:07 +0300

    added new secrets to config

--- flaskServer/config.py
+++ flaskServer/config.py
@@ -1,3 +1,10 @@
 
 # OpenAI API key
 OPEN_AI_SECRET_KEY = "your_secret_key_here"
+
+# Database (postgres) connection details
+DB_NAME = "your_db_name_here"
+DB_PORT = "your_db_port_here"
+DB_IPADDRESS = "your_db_ipaddress_here"
+DB_USERNAME = "your_db_username_here"
+DB_PASSWORD = "your_db_password_here"
