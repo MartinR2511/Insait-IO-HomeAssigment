0c410a39c5e852261d87b333504bfa768760f020
martinroz25@gmail.com <martinroz25@gmail.com>
2024-07-03 21:20:48 +0300

    documented the project in readme file, moved assigment info to a seprate file, added a manual tester for ease of use

---
+++ manual-tester.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+# Define the URL and JSON data
+url = 'http://localhost:5000/ask'
+question = 'what is the capital of France?'
+data = {
+    'question': question,
+}
+
+# Send the POST request with JSON data
+response = requests.post(url, json=data)
+
+# Print the response
+print(response.json())
